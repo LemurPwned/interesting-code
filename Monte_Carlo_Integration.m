93b056703df865435db0869b1082ee946b3c68c8
TOSHIBA-KM\Jakub <jakubmojsiejuk@gmail.com>
2016-10-24 22:32:43 +0200

    Turned into function

--- Monte_Carlo_Integration.m
+++ Monte_Carlo_Integration.m
@@ -1,11 +1,25 @@
-f = @(x, y) x.^3 - 2*(y);
+function integral = Monte_Carlo_Integration(func, x_lower, x_upper, y_lower, y_upper, samples)
+%Monte Carlo integration - integrates function of two variables using monte
+%carlo methods
+%x_lower, x_upper - lower and upper bound of dx integral, 
+%simmilarly for y_upper and y_lower
+%samples- number of samples drawn from the distribution
+f = inline(func, 'x','y');
+% samples = 50000;
+% x_lower = 1;
+% x_upper = 4;
+% y_lower = 2;
+% y_upper = 9;
+% f = @(x, y) x.^2 - 2*(y.^2);
+f = vectorize(f);
 %integrate over region
 %linspace allows for using rectancular domain region
-x = linspace(1,4,50);
-y = linspace(2,9,50);
-z = f(x,y);
+x = linspace(x_lower,x_upper,50);
+y = linspace(y_lower,y_upper,50);
+
 [xm, ym] = meshgrid(x,y);
-zm = xm.^3 - 2*ym;
+zm = f(xm, ym);
+
 
 %bound the drawing space, is tight, but it does not matter, the likelihood
 %of hitting the exact value of function is small
@@ -23,9 +37,9 @@ z_lower_limit = min(zm(:));
 %now draw pairs (x,y) or triples(x_drawn,y_drawn,z_drawn) to mark a point 
 %in space then, check if a function value of e.g z = f(x_drawn,y_drawn) is  
 %is greater or less than the remaining coordinate z_drawn
-draw_x = (x_upper_limit - x_lower_limit).*rand(1,500000) + x_lower_limit;
-draw_y = (y_upper_limit - y_lower_limit).*rand(1,500000) + y_lower_limit;
-draw_z = (z_upper_limit - z_lower_limit).*rand(1,500000) + z_lower_limit;
+draw_x = (x_upper_limit - x_lower_limit).*rand(1,samples) + x_lower_limit;
+draw_y = (y_upper_limit - y_lower_limit).*rand(1,samples) + y_lower_limit;
+draw_z = (z_upper_limit - z_lower_limit).*rand(1,samples) + z_lower_limit;
 
 
 %basically, check if drawn pair (x_drawn,y_drawn) > (x_drawn, f(y_drawn))
@@ -33,6 +47,7 @@ draw_z = (z_upper_limit - z_lower_limit).*rand(1,500000) + z_lower_limit;
 %no point in checking on the whole array of f(x_original), just 
 %validate for f(x_drawn)
 new_z = f(draw_x, draw_y);
+
 hit_p = draw_z(draw_z>0 & new_z>0 & new_z>draw_z);
 hit_n = draw_z(draw_z<0 & new_z<0 & new_z<draw_z);
 
@@ -40,7 +55,7 @@ hit_n = draw_z(draw_z<0 & new_z<0 & new_z<draw_z);
 volume = (x_upper_limit-x_lower_limit)*(y_upper_limit-y_lower_limit)*(z_upper_limit-z_lower_limit);
 %how many hit the integrand space? Calculate ratio
 cover = (size(hit_p,2) - size(hit_n,2))/size(draw_z,2);
-integral = cover*volume
+integral = cover*volume;
 
 hit_p_x = draw_x(draw_z>0 & new_z>0 & new_z>draw_z);
 hit_n_x = draw_x(draw_z<0 & new_z<0 & new_z<draw_z);
@@ -68,3 +83,4 @@ scatter3(hit_n_x, hit_n_y, hit_n, 'r.')
 scatter3(miss_n_x, miss_n_y, miss_n, 'y.')
 scatter3(miss_p_x, miss_p_y, miss_p, 'y.')
 hold off
+%return
